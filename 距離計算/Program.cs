5ecde380ddaa0b34710fe75b5e27d1779cc16dfd
Chiung Ta-Chiu <mentha5681@gmail.com>
2017-07-11 00:45:46 +0800

    Change Save Model

--- 距離計算/Program.cs
+++ 距離計算/Program.cs
@@ -18,25 +18,26 @@ namespace 距離計算
             Console.WriteLine("請輸入轉換文字檔(.csv)之連結地址");
             var add = Console.ReadLine();
             StreamWriter SuccessFile = new StreamWriter("D:\\經緯度轉距離完成檔.txt");
-
             Again:
             try
             {
                 //using (StreamReader SR = new StreamReader(add))
                     var file = new System.IO.StreamReader(add);
                 {
-
-                    string Line;
-
+                    string Line=string.Empty;
+                    Line = file.ReadLine();   //避免欄位名稱寫入
+                    string[] FieldLine = Line.Split(',');
+                    //將欄位名稱寫入txt檔中
+                    SuccessFile.WriteLine(FieldLine[0]+","+ FieldLine[1] + "," + FieldLine[2] + "," + FieldLine[3] + "," + FieldLine[4] +","+ "Distance(m)");
                     while ((Line =file.ReadLine()) != null)
                     {
                         string[] ReadLine_Array = Line.Split(',');
-                        string APIUrl = string.Format($"https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={ReadLine_Array[1]+","+ ReadLine_Array[2]}&destinations={ReadLine_Array[3] + "," + ReadLine_Array[4]}&language=zh-tw&key=AIzaSyDTqOSVYjQX5Pkrt3RBJmSWgGLVmNQiy6k");
+                        string APIUrl = string.Format($"https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={ReadLine_Array[1]+","+ ReadLine_Array[2]}&destinations={ReadLine_Array[3] + "," + ReadLine_Array[4]}&language=zh-tw&key=Your___key");
                         var buffer = new WebClient().DownloadData(APIUrl);
                         string data = Encoding.UTF8.GetString(buffer);
                         var obj = JsonConvert.DeserializeObject<Rootobject>(data);
                         Console.WriteLine("ID:" + ReadLine_Array[0] +"的距離為:"+ obj.rows[0].elements[0].distance.value.ToString() + "公尺");
-                        SuccessFile.WriteLine("ID:" + ReadLine_Array[0] + "的距離為:" + obj.rows[0].elements[0].distance.value.ToString() + "公尺");
+                        SuccessFile.WriteLine(ReadLine_Array[0]+","+ReadLine_Array[1] + "," +ReadLine_Array[2] + "," + ReadLine_Array[3] + "," + ReadLine_Array[4] + "," + obj.rows[0].elements[0].distance.value);
                     }
                     SuccessFile.Close();
                     Console.WriteLine("轉檔完成，請至D:查看");
