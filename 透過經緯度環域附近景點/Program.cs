20d7d6af7931262d8a3d1b8644b72d4e2a50241e
Chiung Ta-Chiu <mentha5681@gmail.com>
2017-07-12 21:58:11 +0800

    update

--- 透過經緯度環域附近景點/Program.cs
+++ 透過經緯度環域附近景點/Program.cs
@@ -16,18 +16,22 @@ namespace 透過經緯度環域附近景點
         {
             Console.WriteLine("請輸入轉換的文字檔(.csv)之連結地址");
             var add = Console.ReadLine();
+            Console.WriteLine("請輸入搜尋範圍半徑(m)");
+            var radius = Console.ReadLine();
+            Console.WriteLine("請輸入搜尋類型，如bus_spot或是restaurant");
+            string type = Console.ReadLine();
             StreamWriter SuccessFile = new StreamWriter("D:\\經緯度鄰近之景點.txt");
             var file = new System.IO.StreamReader(add);
             string Line = string.Empty;
             Line = file.ReadLine();
             SuccessFile.WriteLine(Line + "," + "查詢結果" + "," + "X" + "," + "Y");
-            Line = Get_local_information(SuccessFile, file);
+            Line = Get_local_information(SuccessFile, file , radius,type);
             SuccessFile.Close();
             Console.WriteLine("轉檔已完成!");
             Console.ReadLine();
         }
 
-        private static string Get_local_information(StreamWriter SuccessFile, StreamReader file)
+        private static string Get_local_information(StreamWriter SuccessFile, StreamReader file ,string radius,string type)
         {
             string Line;
             while ((Line = file.ReadLine()) != null)
@@ -35,7 +39,7 @@ namespace 透過經緯度環域附近景點
                 try
                 {
                     string[] ReadLine_Array = Line.Split(',');
-                    string APIUrl = string.Format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={0}&radius={1}&type={2}&key=YOUR_____KEY&language=zh-tw", ReadLine_Array[2] + "," + ReadLine_Array[1], ReadLine_Array[4], ReadLine_Array[5]);
+                    string APIUrl = string.Format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={0}&radius={1}&type={2}&key=your__key&language=zh-tw", ReadLine_Array[2] + "," + ReadLine_Array[1],radius,type);
                     var buffer = new WebClient().DownloadData(APIUrl);
                     string data = Encoding.UTF8.GetString(buffer);
                     var obj = JsonConvert.DeserializeObject<Rootobject>(data);
